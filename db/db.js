cb91c2293873d15aa57fc2fc4ea434b2debcc8d3
SHIVAM2255 <96948986+SHIVAM2255@users.noreply.github.com>
2025-01-07 12:31:48 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 function connectToDb() {
-    mongoose.connect(process.env.DB_CONNECT
+    mongoose.connect('mongodb+srv://shivampatil2107:oKX7O3Tvc6f4dN6V@cluster0.bg9rs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     ).then(() => {
         console.log('Connected to DB');
     }).catch(err => console.log(err));
